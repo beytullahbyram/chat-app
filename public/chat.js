34aa51c09eaeff26da3bc1a4b8562136949606b8
beytullahbyram <beytullah_bayram67@hotmail.com>
2022-03-29 22:20:28 +0300

    düzenleme yapıldı

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,4 @@
-//browser tarafından connectionu tamamlıyoruz
 const socket= io.connect('http://localhost:3000')
-
 
 const sender=document.getElementById('sender');
 const message=document.getElementById('message');
@@ -8,27 +6,20 @@ const btn=document.getElementById('submitBtn');
 const output=document.getElementById('output');
 const feedback=document.getElementById('feedback');
 
-//browserdan click işlemi gelirse bunu serverimızdan socket ile bağlantı kuracağız
 btn.addEventListener('click',()=>{
-    //emit ile mesajları serverımıza gonderiyoruz
     socket.emit('chat',{
         message:message.value,
         sender:sender.value
     })
 })
-
 socket.on('chat',data=>{
-    feedback.innerHTML=''//butona bastıgımızda yaziyor kısmı silinecek
+    feedback.innerHTML=''
     output.innerHTML += '<p><strong>'+data.sender+": "+'</strong>'+data.message+'</p>'
     message.value=''
 })
-
-
-
 message.addEventListener('keypress',()=>{
     socket.emit('typing',sender.value)
 })
-
 socket.on('typing',data=>{
 feedback.innerHTML = '<p>'+data+' yazıyor...</p>'
 })
